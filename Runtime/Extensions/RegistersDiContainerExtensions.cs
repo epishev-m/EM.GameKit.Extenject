bd35faf2c521cd210293dbbe01bf3824a009322e
Maxim Epishev <epishev.m@mail.ru>
2023-06-11 21:11:11 +0300

    Add RegistersDiContainerExtensions

---
+++ Runtime/Extensions/RegistersDiContainerExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace EM.GameKit.Extenject
+{
+
+using Zenject;
+
+public static class RegistersDiContainerExtensions
+{
+	public static DiContainer BindRegisters(this DiContainer container)
+	{
+		container.BindInterfacesTo<Registers>().AsSingle();
+
+		return container;
+	}
+}
+
+}
