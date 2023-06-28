da61f7f91edbd1c8a5bb5ab5df3f7c6685e90984
Maxim Epishev <epishev.m@mail.ru>
2023-06-28 22:47:58 +0300

    Add HintTooltipDiContainerExtensions

---
+++ Runtime/Extensions/HintTooltipDiContainerExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace EM.GameKit.Extenject
+{
+
+using Zenject;
+
+public static class HintTooltipDiContainerExtensions
+{
+	public static DiContainer BindHintTooltip(this DiContainer container)
+	{
+		container.Bind<HintTooltipRouter>().ToSelf().AsSingle();
+		container.Bind<HintTooltipViewModel>().ToSelf().AsSingle();
+
+		return container;
+	}
+}
+
+}
